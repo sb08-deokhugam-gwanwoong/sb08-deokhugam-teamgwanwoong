06e35c67704116409571f6af593311835be23cf3
idktomorrow <swj9675@naver.com>
2026-03-03 14:49:30 +0900

    feat: impl comment repository

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/repository/CommentRepository.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/repository/CommentRepository.java
@@ -1,5 +1,10 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.repository;
 
-public interface CommentRepository {
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 }
