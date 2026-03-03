f815eb3bde3bb81e08edeced8462e07bddde86e3
Junkov0 <devjuncloud@gmail.com>
2026-03-03 14:00:59 +0900

    feat: [RED] add UserRepositoryTest to define user persistence requirements

---
+++ src/test/java/com/codeit/project/sb08deokhugamteamgwanwoong/repository/support/UserRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.codeit.project.sb08deokhugamteamgwanwoong.repository.support;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.User;
+import com.codeit.project.sb08deokhugamteamgwanwoong.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Test
+  @DisplayName("회원가입 테스트: 유저 정보를 저장하고, 이메일로 찾을 수 있어야 한다.")
+  void saveUserAndFindByEmailTest() {
+    // Given
+    User user = User.builder()
+        .email("test@test.com")
+        .nickname("테스터 one")
+        .password("testPass1234!")
+        .build();
+
+    // When
+    User savedUser = userRepository.save(user);
+
+    // Then
+    assertThat(savedUser.getId()).isNotNull();
+    assertThat(savedUser.getEmail()).isEqualTo("test@test.com");
+    assertThat(savedUser.getCreatedAt()).isNotNull();
+  }
+}
