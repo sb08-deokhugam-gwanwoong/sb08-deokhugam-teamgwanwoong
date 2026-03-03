d6b8de4d63d7780443ca6422aa38eb2401234cf8
Junkov0 <devjuncloud@gmail.com>
2026-03-03 13:11:39 +0900

    feat: implement User entity and DTOs

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/user/UserLoginRequest.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/user/UserLoginRequest.java
@@ -1,5 +1,14 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.dto.user;
 
-public record UserLoginRequest() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
+public record UserLoginRequest(
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    @Email(message = "유효한 이메일 형식이 아닙니다.")
+    String email,
+
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    String password
+) {
 }
