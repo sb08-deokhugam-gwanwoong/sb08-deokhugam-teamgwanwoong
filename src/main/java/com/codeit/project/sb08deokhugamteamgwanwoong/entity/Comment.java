7cd10a2dc8440b8dfb58af17786206ffafa1365e
idktomorrow <swj9675@naver.com>
2026-03-03 14:38:02 +0900

    feat: impl comment entity

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/entity/Comment.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/entity/Comment.java
@@ -1,5 +1,38 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.entity;
 
-public class Comment {
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.base.BaseRemovableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@Table(name = "comments")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseRemovableEntity {
+
+  @Column(name = "user_id", nullable = false)
+  private Long userId;
+
+  @Column(name = "review_id", nullable = false)
+  private Long reviewId;
+
+  @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+  private String content;
+
+  @Builder
+  private Comment(Long userId, Long reviewId, String content) {
+    this.userId = userId;
+    this.reviewId = reviewId;
+    this.content = content;
+
+  }
+
+  public void updateContent(String content) {
+    this.content = content;
+  }
 }
