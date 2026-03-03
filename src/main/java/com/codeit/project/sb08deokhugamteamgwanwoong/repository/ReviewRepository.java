5ea113e801766f804ebf9d9f9b60b44dc3ea6f4a
jaejo <jaejoon0520@naver.com>
2026-03-03 11:05:18 +0900

    feat: [RED] impl ReviewRepository save test

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/repository/ReviewRepository.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/repository/ReviewRepository.java
@@ -1,8 +1,22 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.repository;
 
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ReviewRepository {
+import java.util.List;
 
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("SELECT r From Review r JOIN FETCH r.user JOIN FETCH r.book")
+    List<Review> findAll();
+
+    @Query("SELECT r FROM Review r JOIN FETCH r.user JOIN FETCH r.book WHERE r.book.id = :bookId")
+    List<Review> findByBookId(@Param("bookId") Long bookId);
+
+    @Query("SELECT r FROM Review r JOIN FETCH r.user JOIN FETCH r.book WHERE r.book.id = :bookId AND r.user.id = :userId")
+    boolean existsByBookIdAndUserId(@Param("bookId") Long bookId, @Param("userId") Long userId);
 }
