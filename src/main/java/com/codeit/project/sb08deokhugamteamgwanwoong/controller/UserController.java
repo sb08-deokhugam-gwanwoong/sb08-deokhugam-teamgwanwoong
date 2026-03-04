2348fd04f4f6cc4ee258ebf2c084be5188d762ab
Junkov0 <devjuncloud@gmail.com>
2026-03-04 14:30:29 +0900

    feat: [GREEN] UserController add, UserControllerTest User Create Test

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/UserController.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/UserController.java
@@ -1,5 +1,30 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.controller;
 
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.user.UserDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.user.UserRegisterRequest;
+import com.codeit.project.sb08deokhugamteamgwanwoong.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+  private final UserService userService;
+
+  @PostMapping
+  public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserRegisterRequest request) {
+
+    UserDto response = userService.create(request);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
 
 }
