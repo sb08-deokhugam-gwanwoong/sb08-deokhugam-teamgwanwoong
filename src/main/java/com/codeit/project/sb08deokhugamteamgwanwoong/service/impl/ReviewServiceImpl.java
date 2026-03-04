d1b0faee9a807e2ea45141bbff565f6f5927e380
jaejo <jaejoon0520@naver.com>
2026-03-04 14:23:33 +0900

    feat: [GREEN] impl ReviewService - create

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/impl/ReviewServiceImpl.java
@@ -2,6 +2,11 @@ package com.codeit.project.sb08deokhugamteamgwanwoong.service.impl;
 
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.ReviewCreateRequest;
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.ReviewDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Book;
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Review;
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.User;
+import com.codeit.project.sb08deokhugamteamgwanwoong.exception.BusinessException;
+import com.codeit.project.sb08deokhugamteamgwanwoong.exception.ReviewErrorCode;
 import com.codeit.project.sb08deokhugamteamgwanwoong.mapper.ReviewMapper;
 import com.codeit.project.sb08deokhugamteamgwanwoong.repository.BookRepository;
 import com.codeit.project.sb08deokhugamteamgwanwoong.repository.ReviewRepository;
@@ -11,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -26,6 +33,32 @@ public class ReviewServiceImpl implements ReviewService {
     @Override
     @Transactional
     public ReviewDto create(ReviewCreateRequest request) {
+        UUID bookId = request.bookId();
+        UUID userId = request.userId();
 
+        log.info("Service: 리뷰 생성 로직 시작 - bookId: {}, userId: {}", bookId, userId);
+
+        // 이미 작성한 리뷰가 있는 경우
+        if (reviewRepository.existsByBookIdAndUserId(bookId, userId)) {
+            throw new BusinessException(ReviewErrorCode.REVIEW_ALREADY_EXISTS);
+        }
+
+        // 추후 커스텀 에러 만드시면 수정하겠습니다.
+        User user = userRepository.findById(userId)
+                .orElseThrow(IllegalArgumentException::new);
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(IllegalArgumentException::new);
+
+        Review review = Review.builder()
+                .rating(request.rating())
+                .content(request.content())
+                .user(user)
+                .book(book)
+                .build();
+
+        Review savedReview = reviewRepository.save(review);
+        log.info("Service: 리뷰 생성 성공 - ID: {}", savedReview.getId());
+
+        return reviewMapper.toDto(savedReview, false, book.getThumbnailUrl());
     }
 }
