18be68def39c493ea52306962ad5d232705b8717
jaejo <jaejoon0520@naver.com>
2026-03-04 14:18:15 +0900

    feat: [RED] impl ReviewService - create

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/impl/ReviewServiceImpl.java
@@ -1,5 +1,31 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.service.impl;
 
-public class ReviewServiceImpl {
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.ReviewCreateRequest;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.ReviewDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.mapper.ReviewMapper;
+import com.codeit.project.sb08deokhugamteamgwanwoong.repository.BookRepository;
+import com.codeit.project.sb08deokhugamteamgwanwoong.repository.ReviewRepository;
+import com.codeit.project.sb08deokhugamteamgwanwoong.repository.UserRepository;
+import com.codeit.project.sb08deokhugamteamgwanwoong.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class ReviewServiceImpl implements ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final UserRepository userRepository;
+    private final BookRepository bookRepository;
+    private final ReviewMapper reviewMapper;
+
+    @Override
+    @Transactional
+    public ReviewDto create(ReviewCreateRequest request) {
+
+    }
 }
