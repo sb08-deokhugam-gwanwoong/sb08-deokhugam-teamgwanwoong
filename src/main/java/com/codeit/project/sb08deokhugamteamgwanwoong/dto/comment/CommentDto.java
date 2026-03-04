3f49a36085a166c63bc2ae6d31d3e0b07edeee17
idktomorrow <swj9675@naver.com>
2026-03-04 14:18:41 +0900

    feat: add Comment DTO

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/comment/CommentDto.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/comment/CommentDto.java
@@ -1,5 +1,15 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.dto.comment;
 
-public record CommentDto() {
+import java.time.Instant;
+import java.util.UUID;
 
+public record CommentDto(
+    UUID id,
+    UUID reviewId,
+    UUID userId,
+    String userNickname,
+    String content,
+    Instant createdAt,
+    Instant updatedAt
+) {
 }
