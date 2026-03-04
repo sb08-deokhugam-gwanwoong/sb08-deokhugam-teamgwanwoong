023af1f2c671190ee26355ac8ef20268fae6c4c1
Junkov0 <devjuncloud@gmail.com>
2026-03-04 16:21:20 +0900

    refactor: [refactor] Add UserApi and Implementing UserApi in the controller

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/UserController.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/UserController.java
@@ -1,26 +1,24 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.controller;
 
+import com.codeit.project.sb08deokhugamteamgwanwoong.controller.docs.UserApi;
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.user.UserDto;
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.user.UserRegisterRequest;
 import com.codeit.project.sb08deokhugamteamgwanwoong.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-public class UserController {
+public class UserController implements UserApi {
 
   private final UserService userService;
 
-  @PostMapping
-  public ResponseEntity<UserDto> createUser(@RequestBody @Valid UserRegisterRequest request) {
+  @Override
+  public ResponseEntity<UserDto> createUser(UserRegisterRequest request) {
 
     UserDto response = userService.create(request);
 
--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/docs/UserApi.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/docs/UserApi.java
@@ -1,5 +1,46 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.controller.docs;
 
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.user.UserDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.user.UserRegisterRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "사용자 관리", description = "사용자 관련 API")
 public interface UserApi {
 
+  @Operation(
+      summary = "회원가입",
+      description = "새로운 사용자를 등록합니다.",
+      responses = {
+          @ApiResponse(
+              responseCode = "201",
+              description = "회원가입 성공",
+              content = @Content(schema = @Schema(implementation = UserDto.class))
+          ),
+          @ApiResponse(
+              responseCode = "400",
+              description = "잘못된 요청 (입력값 검증 실패)",
+              content = @Content(schema = @Schema(implementation = UserDto.class))
+          ),
+          @ApiResponse(
+              responseCode = "409",
+              description = "이메일 중복",
+              content = @Content(schema = @Schema(implementation = UserDto.class))
+          ),
+          @ApiResponse(
+              responseCode = "500",
+              description = "서버 내부 오류",
+              content = @Content(schema = @Schema(implementation = UserDto.class))
+          )
+      }
+  )
+  @PostMapping
+  ResponseEntity<UserDto> createUser(@RequestBody @Valid UserRegisterRequest request);
 }
