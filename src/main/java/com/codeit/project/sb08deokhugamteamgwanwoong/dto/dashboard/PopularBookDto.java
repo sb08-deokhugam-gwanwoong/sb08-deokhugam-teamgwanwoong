b781f9e769c2d34760dd9d103397885a04ec9ba1
seungwon00 <hsw622555@gmail.com>
2026-03-04 14:49:51 +0900

    feat: Create PopularBookDto

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/dashboard/PopularBookDto.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/dashboard/PopularBookDto.java
@@ -1,5 +1,20 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard;
 
-public record PopularBookDto() {
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+public record PopularBookDto(
+		UUID id,
+		UUID bookId,
+		String title,
+		String author,
+		String thumbnailUrl,
+		String period,
+		Long rank,
+		Double score,
+		Long reviewCount,
+		Double rating,
+		Instant createdAt
+) {
 }
