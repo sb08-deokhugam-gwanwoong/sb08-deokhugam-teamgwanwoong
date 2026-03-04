24781e33f1670ee241dfe471ac4968bab89271f1
jaejo <jaejoon0520@naver.com>
2026-03-05 00:13:03 +0900

    feat: [RED] impl ReviewControllerTest - create

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/ReviewController.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/ReviewController.java
@@ -1,5 +1,29 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.controller;
 
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.ReviewCreateRequest;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.ReviewDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.service.ReviewService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/reviews")
 public class ReviewController {
 
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<ReviewDto> create(
+            @Valid @RequestBody ReviewCreateRequest request
+    ) {
+
+    }
 }
