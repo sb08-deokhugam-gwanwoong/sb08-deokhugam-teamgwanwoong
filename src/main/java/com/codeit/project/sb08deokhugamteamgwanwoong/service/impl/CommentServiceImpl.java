8b1fc191c061b02012ea5a01022e85aee3cda54b
idktomorrow <swj9675@naver.com>
2026-03-04 15:46:51 +0900

    feat: impl comment service

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/impl/CommentServiceImpl.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/impl/CommentServiceImpl.java
@@ -1,5 +1,15 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.service.impl;
 
-public class CommentServiceImpl {
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.comment.CommentCreateRequest;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.comment.CommentDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.service.CommentService;
+import org.springframework.stereotype.Service;
 
+@Service
+public class CommentServiceImpl implements CommentService {
+
+  public CommentDto create(CommentCreateRequest request) {
+
+    return null;
+  }
 }
