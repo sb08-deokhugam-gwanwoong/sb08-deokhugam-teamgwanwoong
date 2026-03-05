f844c6ea9ccf961fd2d066cc0e787bff2fdea30f
idktomorrow <swj9675@naver.com>
2026-03-05 13:00:37 +0900

    feat: impl CommentMapper

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/mapper/CommentMapper.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/mapper/CommentMapper.java
@@ -1,5 +1,16 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.mapper;
 
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.comment.CommentDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Comment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CommentMapper {
 
+  @Mapping(source = "user.id", target = "userId")
+  @Mapping(source = "user.nickname", target = "userNickname")
+  @Mapping(source = "review.id", target = "reviewId")
+  CommentDto toDto(Comment comment);
 }
