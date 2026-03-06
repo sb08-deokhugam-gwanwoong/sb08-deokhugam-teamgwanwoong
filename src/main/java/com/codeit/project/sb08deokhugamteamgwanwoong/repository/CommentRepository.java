c41b7d10828963519d831e08d81b761ca791aaad
idktomorrow <swj9675@naver.com>
2026-03-06 15:24:58 +0900

    feat : add delete query to CommentRepository

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/repository/CommentRepository.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/repository/CommentRepository.java
@@ -1,11 +1,38 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.repository;
 
 import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Comment;
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
+
+  // 특정 리뷰의 댓글 중 삭제되지 않은 것들을 최신순으로 페이징 조회
+  @Query("SELECT c FROM Comment c WHERE c.review.id = :reviewId " +
+      "AND (:cursorCreatedAt IS NULL OR c.createdAt < :cursorCreatedAt) " +
+      "AND c.deletedAt IS NULL " +
+      "ORDER BY c.createdAt DESC")
+  List<Comment> findCommentsByCursor(
+      @Param("reviewId") UUID reviewId,
+      @Param("cursorCreatedAt") Instant cursorCreatedAt,
+      Pageable pageable
+  );
+
+  // @Query 애노테이션은 기본적으로 SELECT 쿼리임으로 @Modifying 애노테이션을 추가하여 조회가 아님을 명시
+  // 메모리에 올려두지 않고 DB에 직접 쿼리 - 데이터 불일치 막음
+  @Modifying(clearAutomatically = true)
+  @Query("UPDATE Comment c SET c.deletedAt = :now WHERE c.review.id = :reviewId AND c.deletedAt IS NULL")
+  void softDeleteAllByReviewId(@Param("reviewId") UUID reviewId, @Param("now") Instant now);
+
+  @Modifying(clearAutomatically = true)
+  @Query("DELETE FROM Comment c WHERE c.review.id = :reviewId")
+  void hardDeleteAllByReviewId(@Param("reviewId") UUID reviewId);
 
 }
