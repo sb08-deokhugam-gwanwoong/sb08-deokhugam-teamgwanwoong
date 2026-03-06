aee7dd9d83a6c31108c416d7de068a7473207e32
seungwon00 <hsw622555@gmail.com>
2026-03-06 15:24:12 +0900

    fix: rename notifications table (content -> message ,review_title -> review_content )

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/mapper/DashboardMapper.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/mapper/DashboardMapper.java
@@ -1,8 +1,10 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.mapper;
 
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PopularBookDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PopularReviewDto;
 import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Book;
 import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Dashboard;
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Review;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -21,4 +23,21 @@ public interface DashboardMapper {
 	@Mapping(source = "book.rating", target = "rating")
 	@Mapping(source = "dashboard.createdAt", target = "createdAt")
 	PopularBookDto toPopularBookDto(Dashboard dashboard, Book book);
+
+	@Mapping(source = "dashboard.id", target = "id")
+	@Mapping(source = "review.id", target = "reviewId")
+	@Mapping(source = "review.book.id", target = "bookId")
+	@Mapping(source = "review.book.title", target = "bookTitle")
+	@Mapping(source = "review.book.thumbnailUrl", target = "bookThumbnailUrl")
+	@Mapping(source = "review.user.id", target = "userId")
+	@Mapping(source = "review.user.nickname", target = "userNickname")
+	@Mapping(source = "review.content", target = "reviewContent")
+	@Mapping(source = "review.rating", target = "reviewRating")
+	@Mapping(source = "dashboard.periodType", target = "period")
+	@Mapping(source = "dashboard.createdAt", target = "createdAt")
+	@Mapping(source = "dashboard.rankingPos", target = "rank")
+	@Mapping(source = "dashboard.score", target = "score")
+	@Mapping(source = "review.likeCount", target = "likeCount")
+	@Mapping(source = "review.commentCount", target = "commentCount")
+	PopularReviewDto toPopularReviewDto(Dashboard dashboard, Review review);
 }
