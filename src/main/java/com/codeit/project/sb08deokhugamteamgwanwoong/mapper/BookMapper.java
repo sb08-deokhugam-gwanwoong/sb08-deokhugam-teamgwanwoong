7109b857abc1f4404dadc5bdbb721fdd9acabca5
홍성휘 <gkemg2017@gmail.com>
2026-03-06 17:12:06 +0900

    refactor: remove useless imports

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/mapper/BookMapper.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/mapper/BookMapper.java
@@ -1,12 +1,8 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.mapper;
 
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.book.BookDto;
-import com.codeit.project.sb08deokhugamteamgwanwoong.dto.book.BookUpdateRequest;
 import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Book;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
