74bf5b6fbb8351002eaeb991a1f26914bc5807a4
seungwon00 <hsw622555@gmail.com>
2026-03-06 17:09:15 +0900

    feat: add PowerUserDto at DashboardService

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/DashboardService.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/DashboardService.java
@@ -2,6 +2,7 @@ package com.codeit.project.sb08deokhugamteamgwanwoong.service;
 
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PopularBookDto;
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PopularReviewDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PowerUserDto;
 import com.codeit.project.sb08deokhugamteamgwanwoong.entity.enums.DashboardPeriodEnums;
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface DashboardService {
 		List<PopularBookDto> getPopularBooks();
 
 		List<PopularReviewDto> getPopularReviews(DashboardPeriodEnums period);
+
+		List<PowerUserDto> getPowerUsers(DashboardPeriodEnums period);
 }
