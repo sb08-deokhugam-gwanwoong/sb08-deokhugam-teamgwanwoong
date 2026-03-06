95404780f3a6a7846138df4dd6bfbb14585067d0
seungwon00 <hsw622555@gmail.com>
2026-03-06 15:26:45 +0900

    feat: add getPopularReviews

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/DashboardService.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/DashboardService.java
@@ -1,8 +1,12 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.service;
 
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PopularBookDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PopularReviewDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.enums.DashboardPeriodEnums;
 import java.util.List;
 
 public interface DashboardService {
 		List<PopularBookDto> getPopularBooks();
+
+		List<PopularReviewDto> getPopularReviews(DashboardPeriodEnums period);
 }
