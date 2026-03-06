d1842f72717f341a7f3cd2a7a4677778ef0a7411
홍성휘 <gkemg2017@gmail.com>
2026-03-06 14:04:00 +0900

    feat: impl BookController
    도서 등록 api

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/BookController.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/controller/BookController.java
@@ -1,5 +1,39 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.controller;
 
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.book.BookCreateRequest;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.book.BookDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.service.BookService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/books")
 public class BookController {
 
+  private final BookService bookService;
+
+  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<BookDto> createBook(
+      @Valid @RequestPart("bookData")BookCreateRequest request,
+      @RequestPart(value = "thumbnailImage", required = false) MultipartFile thumbnailImage
+  ) {
+    boolean hasImage = (thumbnailImage != null && !thumbnailImage.isEmpty());
+
+    log.info("도서 등록 요청 - 도서 제목 : {}, ISBN : {}, hasImage : {}", request.title(), request.isbn(), hasImage);
+    BookDto bookDto = bookService.createBook(request, thumbnailImage);
+    log.info("도서 등록 완료 - 등록된 도서 ID : {}", bookDto.id());
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(bookDto);
+  }
 }
