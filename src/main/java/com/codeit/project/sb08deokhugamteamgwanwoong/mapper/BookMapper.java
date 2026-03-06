b4a28bef294b2a7dfc8088b7fb494702c3bc0482
홍성휘 <gkemg2017@gmail.com>
2026-03-06 15:15:23 +0900

    feat: add updateBookFromRequest() method
    MapStruct를 이용한 도서 수정 메서드

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/mapper/BookMapper.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/mapper/BookMapper.java
@@ -1,12 +1,20 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.mapper;
 
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.book.BookDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.book.BookUpdateRequest;
 import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Book;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BookMapper {
 
     BookDto toDto(Book book);
+
+    // request에서 null인 필드는 무시하고, 값이 있는 것만 Book(엔티티)에 덮어 씌우기
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateBookFromRequest(BookUpdateRequest request, @MappingTarget Book book);
 }
