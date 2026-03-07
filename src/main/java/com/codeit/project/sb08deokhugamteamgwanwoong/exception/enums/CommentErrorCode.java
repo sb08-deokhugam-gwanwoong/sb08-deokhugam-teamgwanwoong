c8affcdea1893616338404f6ae84dee2fd7ff749
idktomorrow <swj9675@naver.com>
2026-03-07 17:02:52 +0900

    feat: add business error codes for comments

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/exception/enums/CommentErrorCode.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/exception/enums/CommentErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CommentErrorCode implements ErrorCode {
   COMMENT_NOT_FOUND(2001, "NOT_FOUND", HttpStatus.NOT_FOUND, "해당 댓글이 존재하지 않습니다."),
+  COMMENT_NOT_OWNER(2002, "FORBIDDEN", HttpStatus.FORBIDDEN, "해당 댓글에 대한 권한이 없습니다."),
   COMMENT_UPDATE_DENIED(2003, "FORBIDDEN", HttpStatus.FORBIDDEN, "사용자 본인의 댓글만 수정할 수 있습니다."),
   COMMENT_DELETE_DENIED(2004, "FORBIDDEN", HttpStatus.FORBIDDEN, "본인의 댓글만 삭제할 수 있습니다.");
 
