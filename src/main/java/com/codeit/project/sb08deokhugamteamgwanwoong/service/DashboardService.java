3f33bb9d9962d9fdbb5a86629c73c1362a8870df
seungwon00 <hsw622555@gmail.com>
2026-03-09 16:18:05 +0900

    feat: add CursorPageDto

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/DashboardService.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/service/DashboardService.java
@@ -1,5 +1,9 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.service;
 
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.CursorPageResponsePopularBookDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.CursorPageResponsePopularReviewDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.CursorPageResponsePowerUserDto;
+import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.DashboardPageRequest;
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PopularBookDto;
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PopularReviewDto;
 import com.codeit.project.sb08deokhugamteamgwanwoong.dto.dashboard.PowerUserDto;
@@ -7,9 +11,15 @@ import com.codeit.project.sb08deokhugamteamgwanwoong.entity.enums.DashboardPerio
 import java.util.List;
 
 public interface DashboardService {
-		List<PopularBookDto> getPopularBooks();
+	List<PopularBookDto> getPopularBooks();
 
-		List<PopularReviewDto> getPopularReviews(DashboardPeriodEnums period);
+	List<PopularReviewDto> getPopularReviews(DashboardPeriodEnums period);
 
-		List<PowerUserDto> getPowerUsers(DashboardPeriodEnums period);
+	List<PowerUserDto> getPowerUsers(DashboardPeriodEnums period);
+
+	CursorPageResponsePopularBookDto getPopularBooks(DashboardPageRequest request);
+
+	CursorPageResponsePopularReviewDto getPopularReviews(DashboardPageRequest request);
+
+	CursorPageResponsePowerUserDto getPowerUsers(DashboardPageRequest request);
 }
