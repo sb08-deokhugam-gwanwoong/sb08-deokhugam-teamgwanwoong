3eb0da0fc395f80c7292df270968c0bc8432e67d
seungwon00 <hsw622555@gmail.com>
2026-03-09 17:49:45 +0900

    feat: create DashboardTargetType

---
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/entity/enums/DashboardTargetType.java
@@ -0,0 +1,10 @@
+package com.codeit.project.sb08deokhugamteamgwanwoong.entity.enums;
+
+/**
+ * 대시보드 랭킹 대상 타입 (DB target_type 컬럼 값과 일치)
+ */
+public enum DashboardTargetType {
+	BOOK,
+	REVIEW,
+	USER
+}
