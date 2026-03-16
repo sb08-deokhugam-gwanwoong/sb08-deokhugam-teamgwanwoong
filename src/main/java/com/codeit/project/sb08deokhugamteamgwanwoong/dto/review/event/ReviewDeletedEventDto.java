87ada8deafb57fbcaaf2724bb3567ff9035b3540
jaejo <jaejoon0520@naver.com>
2026-03-16 10:21:23 +0900

    feat: impl review elasticsearch create, update, delete eventDto

---
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/review/event/ReviewDeletedEventDto.java
@@ -0,0 +1,6 @@
+package com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.event;
+
+import java.util.UUID;
+
+public record ReviewDeletedEventDto(UUID reviewId) {
+}
---
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/review/event/ReviewUpdatedEventDto.java
@@ -0,0 +1,6 @@
+package com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.event;
+
+import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Review;
+
+public record ReviewUpdatedEventDto(Review review) {
+}
