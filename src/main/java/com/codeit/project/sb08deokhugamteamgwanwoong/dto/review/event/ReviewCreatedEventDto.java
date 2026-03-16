b1488367b338e57c094b0a88c2504947421106e7
jaejo <jaejoon0520@naver.com>
2026-03-16 10:33:35 +0900

    refact: refactor ReviewCreatedEventDto, ReviewUpdatedEventDto, ReviewDeletedEventDto

--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/review/event/ReviewCreatedEventDto.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/review/event/ReviewCreatedEventDto.java
@@ -2,5 +2,6 @@ package com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.event;
 
 import com.codeit.project.sb08deokhugamteamgwanwoong.entity.Review;
 
+// 생성자, Getter 생성
 public record ReviewCreatedEventDto(Review review) {
 }
--- src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/review/event/ReviewDeletedEventDto.java
+++ src/main/java/com/codeit/project/sb08deokhugamteamgwanwoong/dto/review/event/ReviewDeletedEventDto.java
@@ -2,5 +2,6 @@ package com.codeit.project.sb08deokhugamteamgwanwoong.dto.review.event;
 
 import java.util.UUID;
 
+// 생성자, Getter 생성
 public record ReviewDeletedEventDto(UUID reviewId) {
 }
