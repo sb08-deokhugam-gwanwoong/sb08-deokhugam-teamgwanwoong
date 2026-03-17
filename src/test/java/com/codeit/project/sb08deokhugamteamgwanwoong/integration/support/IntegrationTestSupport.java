8298a4b8493731844ed8bb5d1a1a8a4555d459af
jaejo <jaejoon0520@naver.com>
2026-03-17 09:43:04 +0900

    feat: impl logstash

--- src/test/java/com/codeit/project/sb08deokhugamteamgwanwoong/integration/support/IntegrationTestSupport.java
+++ src/test/java/com/codeit/project/sb08deokhugamteamgwanwoong/integration/support/IntegrationTestSupport.java
@@ -1,11 +1,13 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.integration.support;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -13,6 +15,9 @@ import org.springframework.test.web.servlet.MockMvc;
 @ActiveProfiles("test")
 @Transactional
 public abstract class IntegrationTestSupport {
+
+  @MockitoBean
+  protected ElasticsearchClient elasticsearchClient;
 
   @Autowired
   protected MockMvc mockMvc;
