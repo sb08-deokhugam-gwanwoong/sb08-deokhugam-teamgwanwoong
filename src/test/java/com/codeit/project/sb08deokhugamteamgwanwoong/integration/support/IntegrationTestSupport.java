d8dc251b0d083a93628ddbc5385dc8a311dcbf88
jaejo <jaejoon0520@naver.com>
2026-03-17 09:48:20 +0900

    fix: modified IntegrationTestSupport

--- src/test/java/com/codeit/project/sb08deokhugamteamgwanwoong/integration/support/IntegrationTestSupport.java
+++ src/test/java/com/codeit/project/sb08deokhugamteamgwanwoong/integration/support/IntegrationTestSupport.java
@@ -6,6 +6,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +19,9 @@ public abstract class IntegrationTestSupport {
 
   @MockitoBean
   protected ElasticsearchClient elasticsearchClient;
+
+  @MockitoBean
+  protected ElasticsearchOperations elasticsearchOperations;
 
   @Autowired
   protected MockMvc mockMvc;
