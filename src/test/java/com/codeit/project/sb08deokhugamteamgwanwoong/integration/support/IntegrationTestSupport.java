a9bf9f6ecbe41301ce732ff89f9b20fde07c8c79
jaejo <jaejoon0520@naver.com>
2026-03-17 10:15:01 +0900

    fix: modified test.yml

--- src/test/java/com/codeit/project/sb08deokhugamteamgwanwoong/integration/support/IntegrationTestSupport.java
+++ src/test/java/com/codeit/project/sb08deokhugamteamgwanwoong/integration/support/IntegrationTestSupport.java
@@ -1,14 +1,11 @@
 package com.codeit.project.sb08deokhugamteamgwanwoong.integration.support;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -17,15 +14,9 @@ import org.springframework.test.web.servlet.MockMvc;
 @Transactional
 public abstract class IntegrationTestSupport {
 
-  @MockitoBean
-  protected ElasticsearchClient elasticsearchClient;
+    @Autowired
+    protected MockMvc mockMvc;
 
-  @MockitoBean
-  protected ElasticsearchOperations elasticsearchOperations;
-
-  @Autowired
-  protected MockMvc mockMvc;
-
-  @Autowired
-  protected ObjectMapper objectMapper;
+    @Autowired
+    protected ObjectMapper objectMapper;
 }
